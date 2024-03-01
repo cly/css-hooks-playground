508a9de1c6d0553dbb43a5e73dbd07216ecced73
Charlie Liang Yuan <djecho@gmail.com>
2024-03-01 11:53:05 -0800

    This actually works

--- src/css.ts
+++ src/css.ts
@@ -2,19 +2,9 @@ import { createHooks } from "@css-hooks/react";
 
 export const [hooks, css] = createHooks({
   light: {
-    or: [
-      "[data-theme='light'] &",
-      {
-        and: ["[data-theme='auto'] &", "@media (prefers-color-scheme: light)"],
-      },
-    ],
+    or: ["[data-theme='light'] &"],
   },
   dark: {
-    or: [
-      "[data-theme='dark'] &",
-      {
-        and: ["[data-theme='auto'] &", "@media (prefers-color-scheme: dark)"],
-      },
-    ],
+    or: ["[data-theme='dark'] &"],
   },
 });
